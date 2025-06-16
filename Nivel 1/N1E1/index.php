4d3d638b6759e9ac8230e20150ba4f680c611330
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-16 19:49:17 +0200

    final update

--- Nivel 1/N1E1/index.php
+++ Nivel 1/N1E1/index.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 require_once 'classes/Employee.php';
 
 $Asier = new Employee("Asier Carrasco", 1000);
-$Asier->shallPayTaxes();
 $Emp1088 = new Employee("Jane Doe", 15000);
-$Emp1088->shallPayTaxes();
 $Emp1089 = new Employee("John Doe", 6000);
+
+$Asier->shallPayTaxes();
+$Emp1088->shallPayTaxes();
 $Emp1089->shallPayTaxes();
+?>
