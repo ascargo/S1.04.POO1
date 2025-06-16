7acaabfcd4df011e983b7e259ae6fa318e5d49bf
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-16 18:57:46 +0200

    peer reviewed update

--- Nivel 1/N1E2/index.php
+++ Nivel 1/N1E2/index.php
@@ -6,7 +6,8 @@ require_once 'classes/Rectangle.php';
 require_once 'classes/Triangle.php';
 
 $shapeOne = new Rectangle(20, 10);
-echo "the area of the rectangle is " . $shapeOne->area() . "\n";
 $shapeTwo = new Triangle(5,2);
+
+echo "the area of the rectangle is " . $shapeOne->area() . "\n";
 echo "The area of the triangle is " . $shapeTwo->area() . "\n";
 ?>
